009215739c4428c1b430894ba29272a4314f26f4
Musthafa <musthafaibnujinna@gmail.com>
2019-12-09 11:49:11 -0800

    initial commit

---
+++ js tutorial/main.js
@@ -0,0 +1,13 @@
+function person (FirsrName ,lastName, dob){
+    this.FirsrName = FirsrName
+    this.lastName = lastName
+    this.dob =dob
+
+}
+
+const Person1 = new person('musthafa','malik','19-03-1997')
+
+console.log(Person1)
+
+  
+
